75bcfa6a194b19f6c49f4cfdc90f9235bbb50fb0
KaiFleenor <kaifleenor@gmail.com>
2025-09-19 00:00:00 +0000

    Finalized platformer game

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -31,18 +31,48 @@ $(function () {
 
 
     // TODO 2 - Create Platforms
-
-
+    createPlatform(1250, 300, 110, 15, "hotpink");
+    createPlatform(600, 495, 160, 15, "cyan");
+    createPlatform(400, 640, 10, 10, "black");
+    createPlatform(550, 570, 20, 170, "cyan")
+    createPlatform(570, 650, 30, 10, "cyan")
+    createPlatform(900, 400, 250, 11, "cyan")
+    createPlatform(1100, 180, 20, 20, "blue")
+    createPlatform(1250, 170, 20, 130, "hotpink")
+    createPlatform(1350, 160, 10, 140, "hotpink")
+    createPlatform(1320, 500, 180, 15, "green")
+    createPlatform(1250, 315, 20, 105, "green")
+    createPlatform(880, 620, 35, 15, "green")
+    createPlatform(400, 370, -80, 10, "lime")
+    createPlatform(600, 280, 200, 20, "lime")
+    createPlatform(750, 600, 10, 140, "cyan")
+    createPlatform(740, 730, 10, 10, "cyan")
+    createPlatform(400, 280, 15, 100, "lime")
+    createPlatform(305, 280, 15, 100, "lime")
+    createPlatform(800, 100, 100, 10, "blue")
+    createPlatform(915, 411, 15, 329)
+    createPlatform(1230, 500, 15, 135)
+    createPlatform(930, 620, 70, 15, "green")
+    createPlatform(1080, 620, -80, 15, "green")
+    createPlatform(1080, 620, 150, 15, "green")
+    createPlatform(970, 180, 20, 20, "blue")
+    createPlatform(200, 150, 100, 20, "red")
 
 
     // TODO 3 - Create Collectables
-
+    createCollectable("kennedi", 650, 650);
+    createCollectable("diamond", 1340, 450);
+    createCollectable("max", 830, 50)
+    createCollectable("grace", 335, 310)
+    createCollectable("steve", 930, 550, 0.5, 1)
 
 
     
     // TODO 4 - Create Cannons
 
-
+    createCannon("right", 300, 1500);
+    createCannon("right", 580, 1350);
+    createCannon("top", 530, 2500)   
     
     
     //////////////////////////////////
